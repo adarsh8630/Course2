aafbb2d9a43520699ef5c54b1b287939613595bc
Adarsh Khandelwal <adarshkhandelwal112@gmail.com>
2020-07-01 21:47:06 +0530

    Controlled Forms

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import Home from './HomeComponent';
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
+import About from './AboutComponent';
 import Contact from './ContactComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
@@ -36,13 +37,22 @@ class Main extends Component {
             );
         }
 
+        const DishWithId = ({match}) => {
+            return(
+                <Dishdetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+                  comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+            );
+          };
+
         return (
             <div>
             <Header />
             <Switch>
                 <Route path="/home" component={HomePage}/>
+                <Route path="/aboutus" component={()=> <About leaders={this.state.leaders}/> }/>
                 <Route exact path="/menu" component = {() => <Menu dishes = {this.state.dishes}/> } />
-                <Route exact path="/contactus" component = {Contact} />
+                <Route path="/menu/:dishId" component = {DishWithId}/>
+                <Route exact path="/contactus" component = {(Contact)} />
                 <Redirect to = "/home"/>
             </Switch>                   
             <Footer/>
